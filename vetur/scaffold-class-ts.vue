32da35f4c0fbfd3b258690081f16ed5c4d1782e4
XiongAmao <onycat@live.cn>
2019-08-12 16:36:31 +0800

    Init repo.

---
+++ vetur/scaffold-class-ts.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+   
+  </div>
+</template>
+
+<script lang="ts">
+  import { Component, Prop, Vue, Emit } from 'vue-property-decorator'
+
+  @Component
+  export default class App extends Vue {
+  }
+</script>
+
+<style scoped>
+ 
+</style>
---
+++ vetur/scaffold.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+   
+  </div>
+</template>
+
+<script>
+  export default {
+    components: {},
+    props: {},
+    data() {
+      return {}
+    },
+    methods: {},
+    created() {
+      
+    }
+  }
+</script>
+
+<style scoped>
+ 
+</style>
