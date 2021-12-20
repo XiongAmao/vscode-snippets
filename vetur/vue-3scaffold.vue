13f95937f4ef3143bb1103630da1b219168a2032
XiongAmao <onycat@live.cn>
2021-12-20 15:36:52 +0800

    feat: add vue3 scaffold

---
+++ vetur/vue-3scaffold.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="${1:App}">
+
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: '${1:App}',
+  components: {},
+  props: {},
+  emits: [],
+  setup() {
+
+    return {}
+  }
+})
+</script>
+
+<style scoped>
+.${1:App} {}
+</style>
